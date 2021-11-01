a9270d5e0fb5f87b701cfa3ce9129557181d1a20
techhero <48436978+godreamt@users.noreply.github.com>
2021-11-01 09:24:39 +0530

    Update Helper.php

--- app/helper/Helper.php
+++ app/helper/Helper.php
@@ -7,13 +7,13 @@ use Haruncpi\LaravelIdGenerator\IdGenerator;
 
 class Helper {
     public static function GenerateId($user, $table, $branchCode=null) {
-        
+
         if($branchCode != null) {
             $prefix = $branchCode.Config::get('app.hosted');
         }else if($user->branch instanceof Branch) {
             $prefix = $user->branch->branchCode.Config::get('app.hosted');
         }else {
-            $prefix = substr(($user->roles ?? ""), 0, 3).Config::get('app.hosted');
+            $prefix = Config::get('app.hosted').substr(($user->roles ?? ""), 0, 3);
         }
         return IdGenerator::generate(['table' => $table, 'length' => 12, 'prefix' => $prefix, 'reset_on_prefix_change' => true]);
     }
